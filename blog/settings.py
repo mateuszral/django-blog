9de5796aad20ecf54a26d12e0a6c8fc1d61f39ac
mateuszral <mral16102000@gmail.com>
2026-02-09 20:49:12 +0100

    create basic post model and admin view

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'web_blog.apps.WebBlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
