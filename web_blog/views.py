d5286c1f665272fd47c335ec7545d2d8a0e47d00
mateuszral <mral16102000@gmail.com>
2026-02-10 14:21:30 +0100

    add 2 views - post list and details

--- web_blog/views.py
+++ web_blog/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the blog index.")
+
+def post_list(request):
+    return HttpResponse("This is the list of blog posts.")
+
+def post_detail(request, post_id):
+    return HttpResponse(f"This is the detail of post {post_id}.")
